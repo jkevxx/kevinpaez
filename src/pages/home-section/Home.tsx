52b01dd768443ae63313b1427e9e5299eef24654
Kevin <kevin.paez1010@gmail.com>
2023-12-15 13:08:15 -0600

    [feature] - photo added

--- src/pages/home-section/Home.tsx
+++ src/pages/home-section/Home.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from 'react-i18next';
 
 import GithubSVG from '../../svg/GithubSVG';
 import LinkedInSVG from '../../svg/LinkedInSVG';
+import { photo } from '../../utils/data';
 import './home.scss';
 
 const Home = () => {
@@ -9,28 +10,33 @@ const Home = () => {
   return (
     <section id="home" className="home">
       <div className="home-content">
-        <h1>{t('home.title')}</h1>
-        <h2>{t('home.subtitle')}</h2>
+        <div className="content-description">
+          <h1>{t('home.title')}</h1>
+          <h2>{t('home.subtitle')}</h2>
 
-        <div className="home-contact">
-          <a
-            className="linkedIn-link"
-            href="https://linkedin.com/in/kevin-paez-gallardo"
-            target="_blank"
-          >
-            <LinkedInSVG />
-          </a>
-          <a
-            className="github-link"
-            href="https://github.com/jkevxx"
-            target="_blank"
-          >
-            <GithubSVG />
-          </a>
-          <a className="cv-btn" href={t('home.cvLink')} target="_blank">
-            <p>{t('home.button')}</p>
-          </a>
+          <div className="home-contact">
+            <a
+              className="linkedIn-link"
+              href="https://linkedin.com/in/kevin-paez-gallardo"
+              target="_blank"
+            >
+              <LinkedInSVG />
+            </a>
+            <a
+              className="github-link"
+              href="https://github.com/jkevxx"
+              target="_blank"
+            >
+              <GithubSVG />
+            </a>
+            <a className="cv-btn" href={t('home.cvLink')} target="_blank">
+              <p>{t('home.button')}</p>
+            </a>
+          </div>
         </div>
+        <figure className="home-photo">
+          <img src={photo.src} alt={photo.alt} />
+        </figure>
       </div>
     </section>
   );
---
+++ src/utils/data.ts
@@ -0,0 +1,4 @@
+export const photo = {
+  src: '/img/kevin-paez-photo.webp',
+  alt: 'Kevin Paez Photo',
+};
